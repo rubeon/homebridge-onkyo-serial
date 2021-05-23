59fd9f472aab7997e61cd5fda60375494da28f58
Eric Williams <eric@subcritical.org>
2021-05-23 19:29:48 +0000

    updated input handline

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,19 +1,26 @@
-import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
-
+import { Service, PlatformAccessory, CharacteristicValue, CharacteristicGetCallback, CharacteristicSetCallback } from 'homebridge';
+// import { PLUGIN_NAME } from './settings';
 import { OnkyoSerialHomebridgePlatform } from './platform';
 
 import Delimiter = require('@serialport/parser-delimiter');
 
+interface Input {
+  code: string;
+  name: string;
+  index: number;
+}
 
-/**
+/*
  * Platform Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
  */
 export class OnkyoSerialPlatformAccessory {
   private service: Service;
-  private televisionService: Service;
-  private speakerService: Service;
+  private inputServices: Service[] = [];
+  // private televisionService: Service;
+  private tvAccessory;
+  // private speakerService: Service;
   private port;
   private parser;
   private muter;
@@ -25,17 +32,30 @@ export class OnkyoSerialPlatformAccessory {
    * You should implement your own code to track the state of your accessory
    */
 
+  private inputs = {
+    '00': 'VCR/DVR',
+    '01': 'CBL/SAT',
+    '02': 'GAME/TV',
+    '03': 'AUX',
+    '10': 'BD/DVD',
+    '20': 'TAPE',
+    '22': 'PHONO',
+    '23': 'CD',
+    '24': 'FM',
+    '25': 'AM',
+    '26': 'TUNER',
+  };
+
   private states = {
     powerOn: false,
     volume: 0,
     isMuted:false,
     brightness: 0,
-    input: 0,
-  };
-
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
+    input: '00', // hex string
+    inputs: [] as Input[],
+    updates: {
+      'SLI': false,
+    },
   };
 
   constructor(
@@ -57,8 +77,14 @@ export class OnkyoSerialPlatformAccessory {
     this.parser = this.port.pipe(new Delimiter({delimiter: [0x1a]}));
     this.parser.on('data', this.handleAnswer.bind(this));
 
+    this.service = this.accessory.getService(this.platform.Service.Television) ||
+      this.accessory.addService(this.platform.Service.Television);
+
+    this.init();
+
     // get the LightBulb service if it exists, otherwise create a new LightBulb service
     // you can create multiple services for each accessory
+    /*
     this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
 
     // set the service name, this is what is displayed as the default name on the Home app
@@ -75,6 +101,7 @@ export class OnkyoSerialPlatformAccessory {
     // register handlers for the Brightness Characteristic
     this.service.getCharacteristic(this.platform.Characteristic.Brightness)
       .onSet(this.setVolume.bind(this));       // SET - bind to the 'setBrightness` method below
+    */
     // try to create a volume knob
     /*
     this.muter = this.accessory.getService(this.platform.Service.
@@ -105,42 +132,14 @@ export class OnkyoSerialPlatformAccessory {
     // this.service.addLinkedService(this.muter);
 
     // try to add the spekaer service?
+    /*
 
     this.televisionService = new this.platform.Service.Television(this.accessory.context.device.displayName, 'televisionService');
     this.televisionService.setCharacteristic(this.platform.Characteristic.ConfiguredName, 'TV LOL');
 
-    /*
-    this.televisionService = this.accessory.getService(this.platform.Service.Television) ||
-      this.accessory.addService(this.platform.Service.Television,  accessory.context.device.displayName + 'televisionService');
-    this.televisionService.setCharacteristic(this.platform.Characteristic.ConfiguredName, this.accessory.context.displayName + " TV");
-
-    // this.televisionService.setCharacteristic(this.platform.Characteristic.SleepDiscoveryMode,
-    this.platform.Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
     */
-    this.televisionService.getCharacteristic(this.platform.Characteristic.Active)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
-
-    this.platform.log.debug('Adding speaker service');
-
-    this.speakerService = this.accessory.getService(this.platform.Service.TelevisionSpeaker) ||
-      this.accessory.addService(this.platform.Service.TelevisionSpeaker, accessory.context.device.displayName + ' Speaker');
-    this.speakerService
-      .setCharacteristic(this.platform.Characteristic.Active, this.platform.Characteristic.Active.ACTIVE)
-      .setCharacteristic(this.platform.Characteristic.VolumeControlType, this.platform.Characteristic.VolumeControlType.ABSOLUTE);
-    this.speakerService.getCharacteristic(this.platform.Characteristic.Volume)
-      .onSet(this.setVolume.bind(this))
-      .onGet(this.getVolume.bind(this));
-    this.speakerService
-      .setCharacteristic(this.platform.Characteristic.Active, this.platform.Characteristic.Active.ACTIVE)
-      .setCharacteristic(this.platform.Characteristic.VolumeControlType, this.platform.Characteristic.VolumeControlType.ABSOLUTE);
-    this.speakerService.getCharacteristic(this.platform.Characteristic.Mute)
-      .onSet(this.setMuted.bind(this))
-      .onGet(this.getMuted.bind(this));
-
-    this.speakerService.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.displayName + ' Speaker');
-
-    this.televisionService.addLinkedService(this.speakerService);
+    this.platform.log.debug('XXXXX:  this.accessory.context.device.displayName');
+    this.platform.log.debug('XXXXX: ', this.accessory.context.device.displayName);
 
     /**
      * Creating multiple services of the same type.
@@ -171,9 +170,15 @@ export class OnkyoSerialPlatformAccessory {
      * the `updateCharacteristic` method.
      *
      */
+    /*
+    setInterval(() => {
+      this.syncState();
+    }, 10000);
+    */
 
     /*
     let motionDetected = false;
+
     setInterval(() => {
       // EXAMPLE - inverse the trigger
       motionDetected = !motionDetected;
@@ -192,9 +197,174 @@ export class OnkyoSerialPlatformAccessory {
    * Handle "SET" requests from HomeKit
    * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
    */
-  async setOn(value: CharacteristicValue) {
+
+  async init() {
+    await this.createTVService();
+    await this.createTVSpeakerService();
+    await this.createInputSourceServices();
+    // this.platform.api.publishExternalAccessories(PLUGIN_NAME, [this.accessory]);
+    // get the current power state
+  }
+
+  async createTVService() {
+    this.service
+      .setCharacteristic(
+        this.platform.Characteristic.ConfiguredName,
+        this.accessory.context.device.displayName,
+      )
+      .setCharacteristic(
+        this.platform.Characteristic.SleepDiscoveryMode,
+        this.platform.Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE,
+      );
+    this.service.getCharacteristic(this.platform.Characteristic.Active)
+      .on('get', this.getOn.bind(this))
+      .on('set', this.setOn.bind(this));
+
+    this.service
+      .getCharacteristic(this.platform.Characteristic.ActiveIdentifier)
+      .on('get', this.getInputState.bind(this))
+      .on('set', this.setInputState.bind(this));
+  }
+
+  async createTVSpeakerService() {
+    this.platform.log.debug('createTVSpeakerService entered');
+    const speakerService = this.accessory.getService(this.platform.Service.TelevisionSpeaker) ||
+      this.accessory.addService(this.platform.Service.TelevisionSpeaker,
+        this.accessory.context.device.displayName + ' Speaker');
+    speakerService
+      .setCharacteristic(
+        this.platform.Characteristic.ConfiguredName,
+        this.accessory.context.device.displayName + ' Speaker')
+      .setCharacteristic(
+        this.platform.Characteristic.Active,
+        this.platform.Characteristic.Active.ACTIVE)
+      .setCharacteristic(
+        this.platform.Characteristic.VolumeControlType,
+        this.platform.Characteristic.VolumeControlType.ABSOLUTE,
+      );
+
+    // handle volume control
+    speakerService.getCharacteristic(this.platform.Characteristic.VolumeSelector)
+      .on('set', this.setVolume.bind(this))
+      .on('get', this.getVolume.bind(this));
+    speakerService.getCharacteristic(this.platform.Characteristic.Mute)
+      .on('set', this.setMuted.bind(this))
+      .on('get', this.getMuted.bind(this));
+    return;
+  }
+
+  async getInputState(callback: CharacteristicGetCallback) {
+    this.states.updates['SLI'] = true; // hack?
+    this.sendCmd('SLIQSTN');
+    // this.states.input = '00';
+    this.platform.log.debug('getInputState entered');
+    this.platform.log.debug(this.states.input);
+    const input: Input|undefined = this.inputs[this.states.input];
+    this.platform.log.debug('current input: ', input);
+
+    if (!input) {
+      return;
+    }
+
+    this.states.inputs.forEach((input, index) => {
+      if (input.code === this.states.input) {
+        return callback(null, index);
+      } else {
+        this.platform.log.debug('input.code: ', input.code, 'this.states.input: ', this.states.input);
+      }
+      return;
+    });
+  }
+
+  updateInputs(response) {
+    this.platform.log.debug('updateInputs entered');
+    if (this.states.updates['SLI']) {
+      this.platform.log.debug('response expected...');
+      this.states.updates['SLI'] = false;
+      this.states.input = response;
+    } else {
+      this.platform.log.debug('ignoring SLI message');
+      this.platform.log.debug('response', response);
+    }
+  }
+
+  async syncState() {
+    this.sendCmd('PWRQSTN');
+    this.sendCmd('AMTQSTN');
+    this.sendCmd('MVLQSTN');
+    this.sendCmd('SLPQSTN');
+    this.sendCmd('DIFQSTN');
+    this.sendCmd('DIMQSTN');
+    this.sendCmd('SLIQSTN');
+    this.sendCmd('RESQSTN');
+    this.sendCmd('TUNQSTN');
+    this.sendCmd('PRSQSTN');
+    this.platform.log.debug('Input: ', this.states.input);
+  }
+
+  async setInputState(value: CharacteristicValue, callback: CharacteristicSetCallback) {
+    this.platform.log.debug('setInputState entered');
+    // this.states.input = value.toString().padStart(2, '0');
+    // get the ID?
+    this.platform.log.debug('value: ', value);
+    this.platform.log.debug('input code', this.states.inputs[value as number]);
+    // const hexInput = this.states.input.toString(16).toUpperCase();
+    this.states.input = this.states.inputs[value as number].code;
+    this.sendCmd('SLI' + this.states.input);
+    callback(null);
+  }
+
+  async createInputSourceServices() {
+    // will need to add some input sources manually here...
+    this.platform.log.debug('createInputSourceServices entered');
+    this.states.inputs = [];
+    let i = 0;
+    for (const [key, value] of Object.entries(this.inputs)) {
+      this.platform.log.debug('inputs: ', key, value);
+      this.states.inputs.push({
+        code: key,
+        name: value,
+        index: i,
+      });
+      i++;
+    }
+    this.platform.log.debug(JSON.stringify(this.states.inputs));
+
+    this.states.inputs.forEach(input => {
+      this.platform.log.debug('input: ', input);
+      const inputService = this.accessory.addService(
+        this.platform.Service.InputSource,
+        this.platform.api.hap.uuid.generate(input.code + input.name),
+        input.name);
+
+      inputService
+        .setCharacteristic(this.platform.Characteristic.Identifier, input.index)
+        .setCharacteristic(this.platform.Characteristic.Name, input.name)
+        .setCharacteristic(
+          this.platform.Characteristic.IsConfigured,
+          this.platform.Characteristic.IsConfigured.CONFIGURED,
+        )
+        .setCharacteristic(
+          this.platform.Characteristic.CurrentVisibilityState,
+          this.platform.Characteristic.CurrentVisibilityState.SHOWN,
+        )
+        .setCharacteristic(
+          this.platform.Characteristic.InputSourceType,
+          this.platform.Characteristic.InputSourceType.APPLICATION,
+        )
+        .setCharacteristic(
+          this.platform.Characteristic.InputDeviceType,
+          this.platform.Characteristic.InputDeviceType.TV,
+        );
+
+      this.service.addLinkedService(inputService);
+      this.inputServices.push(inputService);
+    });
+  }
+
+  async setOn(state: CharacteristicValue, callback: CharacteristicSetCallback) {
     // implement your own code to turn your device on/off
-    this.states.powerOn = value as boolean;
+    this.states.powerOn = state as boolean;
 
     if (this.states.powerOn) {
       this.platform.log.debug('Turning on...');
@@ -204,7 +374,8 @@ export class OnkyoSerialPlatformAccessory {
       this.sendCmd('PWR00');
     }
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+    this.platform.log.debug('Set Characteristic On ->', state);
+    callback(null);
   }
 
   /**
@@ -234,18 +405,11 @@ export class OnkyoSerialPlatformAccessory {
 
   }
 
-  async getOn(): Promise<CharacteristicValue> {
+  getOn(callback: CharacteristicGetCallback) {
     // implement your own code to check if the device is on
-    const isOn = this.states.powerOn;
-
     this.sendCmd('PWRQSTN');
-
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // if you need to return an error to show the device as 'Not Responding' in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn;
+    callback(null, this.states.powerOn);
+    this.platform.log.debug('Get Characteristic On ->', this.states.powerOn);
   }
 
   isMuted(response) {
@@ -258,15 +422,15 @@ export class OnkyoSerialPlatformAccessory {
     }
   }
 
-  async getMuted(): Promise<CharacteristicValue> {
+  async getMuted(callback: CharacteristicGetCallback) {
     // check if the device is muted
     const isMuted = this.states.isMuted;
     this.sendCmd('AMTQSTN');
     this.platform.log.debug('Get Characteristic On ->', isMuted);
-    return isMuted;
+    callback(null, isMuted);
   }
 
-  async setMuted(value: CharacteristicValue) {
+  async setMuted(value: CharacteristicValue, callback: CharacteristicGetCallback) {
     this.states.isMuted = value as boolean;
     if (this.states.isMuted) {
       this.platform.log.debug('Muting...');
@@ -275,6 +439,7 @@ export class OnkyoSerialPlatformAccessory {
       this.platform.log.debug('Unmuting...');
       this.sendCmd('AMT00');
     }
+    callback(null);
 
   }
 
@@ -302,7 +467,7 @@ export class OnkyoSerialPlatformAccessory {
     this.platform.log.debug('new volume:', this.states.volume);
   }
 
-  async setVolume(value: CharacteristicValue) {
+  async setVolume(value: CharacteristicValue, callback: CharacteristicSetCallback) {
     // applies the configured volume
     const newVolume = value as number;
     this.platform.log.debug('Setting volume to', newVolume, '%');
@@ -313,7 +478,7 @@ export class OnkyoSerialPlatformAccessory {
     // convert to hex
     const hexVolume = realVolume.toString(16).toUpperCase();
     this.sendCmd('MVL' + hexVolume);
-
+    callback(null);
   }
 
   sendCmd(cmd) {
@@ -343,6 +508,9 @@ export class OnkyoSerialPlatformAccessory {
       case 'MVL':
         this.updateVolume(responseArgs);
         break;
+      case 'SLI':
+        this.platform.log.debug('input selector response');
+        this.updateInputs(responseArgs);
     }
   }
 
